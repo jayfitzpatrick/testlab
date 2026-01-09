138f7d8aec15ca25c1256155f8087ad9b5dd70f0
Jason Fitzpatrick <jayfitzpatrick@gmail.com>
2026-01-09 11:51:08 +0000

    Add One line installer

--- deploy.sh
+++ deploy.sh
@@ -9,19 +9,19 @@ cd $workdir || exit
 
 
 echo "Update the package list and install system upgrades"
-# sudo apt update && sudo apt upgrade -y
+sudo apt update && sudo apt upgrade -y
 
 echo "Installing Docker dependencies and adding Docker's official GPG key and repository"
-# sudo apt install -y ca-certificates curl gnupg lsb-release
+sudo apt install -y ca-certificates curl gnupg lsb-release
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg --yes
 
 echo "Setting up the Docker repository"
 sudo echo  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-# sudo apt update
+sudo apt update
 
 echo "Installing Docker Engine and Docker Compose"
-# sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 echo "Enabling and starting Docker service"
 sudo systemctl enable docker
